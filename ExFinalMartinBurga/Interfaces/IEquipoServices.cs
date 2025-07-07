34b583752b01a384c39f94b9dab5ae6b5ebc4bf6
MartinBurga <martin.burga@udla.edu.ec>
2025-07-07 07:37:04 -0500

    Creacion de la interface de EquipoServices e implementacion en su Repository con metodos genericos

---
+++ ExFinalMartinBurga/Interfaces/IEquipoServices.cs
@@ -0,0 +1,18 @@
+﻿using ExFinalMartinBurga.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFinalMartinBurga.Interfaces
+{
+    interface IEquipoServices
+    {
+        public void Init();
+        public Task<List<Equipo>> DevuelveEquipos();
+        public Task<bool> GuardarEquipos(Equipo equipo);
+        public Task<bool> EliminarEquipos(int id);
+
+    }
+}
---
+++ ExFinalMartinBurga/Repositories/EquipoRepository.cs
@@ -0,0 +1,37 @@
+﻿using ExFinalMartinBurga.Interfaces;
+using ExFinalMartinBurga.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFinalMartinBurga.Repositories
+{
+    class EquipoRepository : IEquipoServices
+    {
+        private SQLiteAsyncConnection _connection;
+        private string _dbPath = FileSystem.AppDataDirectory + "/equipos.db3";
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<Equipo>> DevuelveEquipos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EliminarEquipos(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GuardarEquipos(Equipo equipo)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
