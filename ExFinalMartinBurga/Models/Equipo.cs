c3f7f5c704b1670090aba6849fe44ee174839443
MartinBurga <martin.burga@udla.edu.ec>
2025-07-07 07:23:33 -0500

    Creacion de carpetas para un correcta distribucion y creacion de modelo "Equipo"

---
+++ ExFinalMartinBurga/Models/Equipo.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFinalMartinBurga.Models
+{
+    [Table("Equipo")]
+    class Equipo
+    {
+        [AutoIncrement: PrimaryKey]
+        string Dispositivo { get; set; }
+        [NotNull]
+        string Marca { get; set; }
+        [NotNull]
+        bool GarantiaActiva { get; set; }
+        [NotNull]
+        int VidaUtil { get; set; }
+        [NotNull]
+
+    }
+}
